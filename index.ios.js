beb4f04fd27725b53af04d7625b63a9baa415f49
thanhcs94 <nguyenvanthanh9294@gmail.com>
2016-11-22 23:22:48 +0700

    thanhcs94: first in react-native

---
+++ index.ios.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class ThanhStore extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.thanhdt}>
+          Thanh Dep Trai !
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  thanhdt: {
+    fontSize: 20,
+    color: '#006789',
+    textAlign: 'center',
+    margin: 10,
+  }
+});
+
+AppRegistry.registerComponent('ThanhStore', () => ThanhStore);
